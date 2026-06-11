eed4be5e8f4b38b3507da1b92bdc4618cfcb824f
Aquiles <200628218@student.georgianc.on.ca>
2026-06-11 19:35:41 -0400

    Starter code for project uploaded.

---
+++ SoccerPitch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoccerPitch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
