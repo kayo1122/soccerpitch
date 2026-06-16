fb29bfc37593c4808176d36f2ea6b23a3bcdd3d8
Aquiles <200628218@student.georgianc.on.ca>
2026-06-16 10:55:13 -0400

    migrated models, setup application db context, setup azure model/tables.

--- SoccerPitch/Data/ApplicationDbContext.cs
+++ SoccerPitch/Data/ApplicationDbContext.cs
@@ -3,7 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace SoccerPitch.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<SoccerPitch.Models.User> Users { get; set; } = default;
+
+        public DbSet<SoccerPitch.Models.Team> Teams { get; set; } = default;
+
+        public DbSet<SoccerPitch.Models.Player> Players { get; set; } = default;
     }
 }
--- SoccerPitch/Models/Team.cs
+++ SoccerPitch/Models/Team.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
 
-namespace SoccerPitch1.Models
+namespace SoccerPitch.Models
 {
     public class Team
     {
